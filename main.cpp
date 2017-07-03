233c9068bd9048f5d3ffa3c54f572494a5538f4a
Tama <t.mcglinn@gmail.com>
2017-07-03 15:27:55 +0000

    get reference image; diff current frame layer by layer

--- main.cpp
+++ main.cpp
@@ -1,37 +1,62 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <vector>
 #include <ctime>
 #include <iostream>
 #include <raspicam/raspicam_cv.h>
-using namespace std; 
+using std::cout;
+using std::cerr;
+using std::endl;
+
+const char * window_title = "GuardDog";
+
+const int numberOfSteps = 2;
+cv::Mat images[numberOfSteps-1];
+std::vector<cv::Mat> frame_split(3);
+std::vector<cv::Mat> reference_split(3);
+
+struct CameraWrapper {
+  CameraWrapper() {}
+  ~CameraWrapper() { cout << "Camera released"; cam.release(); }
+	raspicam::RaspiCam_Cv cam;
+};
 
 int main ( int argc,char **argv ) {
-   
-    time_t timer_begin,timer_end;
-    raspicam::RaspiCam_Cv Camera;
-    cv::Mat image;
-    int nCount=100;
-    //set camera params
-    Camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 );
-    //Open camera
-    cout<<"Opening Camera..."<<endl;
-    if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
-    //Start capture
-    cout<<"Capturing "<<nCount<<" frames ...."<<endl;
-    time ( &timer_begin );
-    for ( int i=0; i<nCount; i++ ) {
-        Camera.grab();
-        Camera.retrieve ( image);
-        if ( i%5==0 )  cout<<"\r captured "<<i<<" images"<<std::flush;
-    }
-    cout<<"Stop camera..."<<endl;
-    Camera.release();
-    //show time statistics
-    time ( &timer_end ); /* get current time; same as: timer = time(NULL)  */
-    double secondsElapsed = difftime ( timer_end,timer_begin );
-    cout<< secondsElapsed<<" seconds for "<< nCount<<"  frames : FPS = "<<  ( float ) ( ( float ) ( nCount ) /secondsElapsed ) <<endl;
-    //save image 
-    cv::imwrite("raspicam_cv_image.jpg",image);
-    cout<<"Image saved at raspicam_cv_image.jpg"<<endl;
+	{
+	CameraWrapper cameraWrapper;
+	raspicam::RaspiCam_Cv & Camera = cameraWrapper.cam;
+
+	//set camera params
+	Camera.set( CV_CAP_PROP_FORMAT, CV_8UC3 );
+
+	//Open camera
+	if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
+
+	cv::namedWindow(window_title,1); //0 required for FULLSCREEN, 1 is normal (autosize)
+
+	do {
+		Camera.grab();
+		Camera.retrieve(images[0]);
+	} while(cv::waitKey(30) != 32); //first spacebar; get ref image
+
+	cv::split(images[0],reference_split);
+
+	//Start capture
+	do {
+		Camera.grab();
+		Camera.retrieve(images[0]);
+
+		cv::split(images[0], frame_split);
+		for(int channel = 0; channel < 3; channel++){
+			cv::absdiff(frame_split.at(channel), reference_split.at(channel), frame_split.at(channel));
+			cv::threshold(frame_split.at(channel), frame_split.at(channel), 15, 255, CV_THRESH_BINARY);
+			merge(frame_split, images[0]);
+		}
+
+		cv::imshow(window_title, images[0]);
+	} while( cv::waitKey(30) != 32 ); //spacebar
+	}
+
+	return 0;
 }
-
-
 
