bbda03d3e9734c41cc811d935dc42d8b467f11a5
Tama <t.mcglinn@gmail.com>
2017-07-04 19:41:41 +0200

    Show frame when it differs to reference

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using std::endl;
 
 const char * window_title = "GuardDog";
 
-cv::Mat image;
+cv::Mat image, reference_image, grayscaled_image, snapshot;
 
 struct CameraWrapper {
   CameraWrapper() {}
@@ -24,51 +24,47 @@ int main ( /*int argc,char **argv*/ ) {
 	raspicam::RaspiCam_Cv & Camera = cameraWrapper.cam;
 
 	//set camera params
-	Camera.set( CV_CAP_PROP_FORMAT, CV_8UC1 );
+	Camera.set( CV_CAP_PROP_FORMAT, CV_8UC3 );
 
 	//Open camera
 	if (!Camera.open()) {cerr<<"Error opening the camera"<<endl;return -1;}
 
 	cv::namedWindow(window_title,1); //0 required for FULLSCREEN, 1 is normal (autosize)
 
-	cv::Mat reference_image;
 	do {
 		Camera.grab();
-		Camera.retrieve(reference_image);
-	} while(cv::waitKey(30) != 32); //first spacebar; get ref image
+		Camera.retrieve(image);
+		cv::cvtColor(image, reference_image, CV_BGR2GRAY);
+		cv::imshow(window_title, reference_image);
+	} while( cv::waitKey(30) != 32); // spacebar
 
 	unsigned int frame_num = 0;
 	float diff_value;
-	bool recording = true;
-
-	cout << reference_image.size() << endl;
-	int videoCodec = CV_FOURCC('D','I','V','X');
-	double fps = 30;
-	cv::VideoWriter outputVideo("output.avi", videoCodec, fps, reference_image.size(), true);
-	if(!outputVideo.isOpened()) {cerr<<"ERROR: Failed to write the video"<<endl;return -1;}
 
 	do {
 		Camera.grab();
 		Camera.retrieve(image);
 
-		cv::absdiff(image, reference_image, image);
+		cv::cvtColor(image, grayscaled_image, CV_BGR2GRAY);
 
-		cv::imshow(window_title, image);
+		cv::absdiff(grayscaled_image, reference_image, grayscaled_image);
 
-    cv::normalize(image, image, 0, 255, 32, CV_8UC1);
+    cv::normalize(grayscaled_image, grayscaled_image, 0, 255, 32, CV_8UC1);
 
-		cv::Scalar m = cv::mean(image);
+		cv::Scalar m = cv::mean(grayscaled_image);
 		diff_value = m[0];
 		if( frame_num % 10 == 0 ){
     	cout << "\rDiff: " << diff_value << std::flush;
     }
 
-		outputVideo.write(image);
+    if( diff_value > 30.0f ){
+			cv::imshow(window_title, image);
+		} else {
+			cv::imshow(window_title, grayscaled_image);
+		}
 
     ++frame_num;
 	} while( cv::waitKey(30) != 32 ); //spacebar
-
-	outputVideo.release();
 
 	cout << endl;
 
