1df092369e50e8dbaeb55fd6965c04643bb3a89c
Tama <t.mcglinn@gmail.com>
2017-07-15 12:16:24 +0200

    index.php - to list images

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <html>
 <body>
-<!-- user interface code begins -->
-<button value="up">↑</button>
-<button value="down">↓</button>
-<button value="left">←</button>
-<button value="right">→</button>
-<!-- user interface code ends -->
+
+<?php
+$hourly_images="{$_SERVER['DOCUMENT_ROOT']}/GuardDog/images/hourly/"
+foreach(glob(hourly_images.'*') as $filename){
+    echo basename($filename) . "\n";
+}
+?>
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 <script type="text/javascript">
@@ -14,14 +15,13 @@ var phpScript='controller.php';
 var keyPressed='';
 
 	$("button").click(function(){
-		
 		keyPressed=$(this).val(); // get value of key pressed
-		
 		$.post(phpScript,{data:keyPressed},function(response){
 			alert(response);
 		});
 	});
 });
+
 </script>
 </body>
 </html>
