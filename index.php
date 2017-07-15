66f77a5a2a293c111a7bc18cf9af6b89042b79cf
Tama <t.mcglinn@gmail.com>
2017-07-15 14:18:08 +0200

    Basic version that lists the images

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
 <body>
 
 <?php
-$hourly_images="{$_SERVER['DOCUMENT_ROOT']}/GuardDog/images/hourly/"
-foreach(glob(hourly_images.'*') as $filename){
-    echo basename($filename) . "\n";
+
+function getImages($dir){
+		$result = array();
+		$docRoot = $_SERVER["DOCUMENT_ROOT"] . '/';
+		$dir = $docRoot . $dir;
+		$fullFilenames = glob($dir.'*');
+		foreach($fullFilenames as $fullFilename){
+			$result[] = substr($fullFilename, strlen($docRoot));
+		}
+		return $result;
 }
+
+echo '<div class=imagecollection>';
+echo "<h2>Past hourly pictures</h2>";
+$hourly_images = getImages("GuardDog/images/hourly/");
+foreach($hourly_images as $img) {
+  echo '<img class="photo grow" src="' . $img . '" />';
+}
+echo '</div>';
+
+echo "<br />";
+
+echo '<div class=imagecollection>';
+echo "<h2>Recent events</h2>";
+$images = getImages("GuardDog/images/events/");
+foreach($images as $img) {
+  echo '<img class="photo grow" src="' . $img . '" />';
+}
+echo '</div>';
+
 ?>
 
 </script>
